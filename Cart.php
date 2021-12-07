bbba8bb8229eb4c2c05548b02a075f3042d22338
Chloe Bril <s1171348@student.windesheim.nl>
2021-12-07 13:56:56 +0100

    US2 gefixt

--- Cart.php
+++ Cart.php
@@ -15,9 +15,13 @@ echo createTable($cart, $databaseConnection);
 
 function createTable($cart, $databaseConnection)
 {
-    $table = "";
-    $total = 0;
     $totalamount = 0;
+    $table = null;
+    if(count($cart) != 0) {
+        $table = "<h1>Inhoud Winkelwagen</h1>
+                  <table border='2'><tr><th>Productnaam</th><th>Afbeelding</th><th>Aantal</th><th>Prijs</th><tr><tr>";
+    }
+    $total = 0;
 
     foreach ($cart as $id => $item) {
         if(isset($_GET[$id])){
@@ -35,9 +39,7 @@ function createTable($cart, $databaseConnection)
             $total += $price;
             $totalamount += $amount;
 
-            $table .= "<h1>Inhoud Winkelwagen</h1>
-                    <table border='2'><tr> <th>Productnaam</th><th>Afbeelding</th><th>Aantal</th><th>Prijs</th><tr>
-                    <tr>
+            $table .= "
                     <th><a href='http://localhost/nerdygadgets/view.php?id=$id'>$StockItemName</a></th>
                     <th><img src='Public/StockItemIMG/$StockItemPath' width='100' alt='Product afbeelding'></th>
                     <th><form method='GET'><input type='number' name='amount' value='$amount'
@@ -48,11 +50,12 @@ function createTable($cart, $databaseConnection)
                </tr>";
         }
     }
+
     $total = round($total, 2);
     if($totalamount == 0) {
         print("<br><H1><center>Je winkelmand is leeg. <a href='http://localhost/nerdygadgets'><u>Shop nu!</u></a></center></H1>");
     }
-    if(!$totalamount == 0) {
+    else {
         $table .= "<tr><th>Totaal:</th><th><!--afbeelding--></th><th>$totalamount</th><th>$total</th></table>
     <form method='get' action='redirectPaymentChoice.php'>
         <input style='height: 48px; width: 240px' type='submit' name='submit' value='Bestellen'> </form>";
