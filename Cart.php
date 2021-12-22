e48e066bd54885822f2315b5bdd70820a74eaede
Chloe Bril <s1171348@student.windesheim.nl>
2021-12-22 10:01:08 +0100

    cart bestelknop gefixt

--- Cart.php
+++ Cart.php
@@ -30,7 +30,7 @@ if (count($cart) > 0) {
         <th>Prijs</th>
     <tr>";
 } else {
-    echo "<br><h4 style='text-align: center;'>Je winkelmand is leeg. <a href='http://localhost/nerdygadgets'><u>Shop nu!</u></a></h4>";
+    echo "<br><h1 class='empty-cart'>Je winkelmand is leeg. <a href='http://localhost/nerdygadgets'><u>Shop nu!</u></a></h1>";
 }
 
 // This loop creates a table row for every product
@@ -87,15 +87,17 @@ if (count($cart) > 0) {
 }
 
 
-if (isset($_SESSION['email'])) {
-    echo "<form method='GET' action='Payment.php' class='CartOrderButton'>
+if ($totalAmount != 0) {
+    if (isset($_SESSION['email'])) {
+        echo "<form method='GET' action='Payment.php' class='CartOrderButton'>
             <input type='text' name='value' value='$value' hidden>
             <input style='height: 48px; width: 240px' type='submit' name='submit' value='Bestellen'> 
         </form>";
-} else {
-    echo "<form method='GET' action='Login.php' class='CartOrderButton'>
+    } else {
+        echo "<form method='GET' action='Login.php' class='CartOrderButton'>
             <input style='height: 48px; width: 240px; margin-top: 15px; border-radius: 10px' type='submit' name='submit' value='Bestellen'>
           </form>";
+    }
 }
 ?>
 </body>
