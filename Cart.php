86286f068219b341f114b5231130c0bad2593609
Maarten Drost <drostmaarten05@gmail.com>
2021-12-22 10:59:45 +0100

    US14 weer testen

--- Cart.php
+++ Cart.php
@@ -113,50 +113,48 @@ if (isset($_SESSION['email'])) {
             <input style='height: 48px; width: 240px; margin-top: 15px; border-radius: 10px' type='submit' name='submit' value='Bestellen'>
           </form>";
 }
-
 ?>
 
-<h1 style='text-align:center'>Misschien is dit ook wat voor U?</h1>
-
 <?php
-$id = $_SESSION['id'];
-$sql = "SELECT StockGroupID FROM stockitemstockgroups WHERE StockItemID ='$id'";
+if(!empty($_SESSION['id'])) {
+    echo"<h1 style='text-align:center'>Misschien is dit ook wat voor U?</h1>";
+    $id = $_SESSION['id'];
+    $sql = "SELECT StockGroupID FROM stockitemstockgroups WHERE StockItemID ='$id'";
 
-$Statement = mysqli_prepare($databaseConnection, $sql);
-mysqli_stmt_execute($Statement);
-$ReturnableResult = mysqli_stmt_get_result($Statement);
-$record = mysqli_fetch_assoc($ReturnableResult);
-$category = $record['StockGroupID'];
+    $Statement = mysqli_prepare($databaseConnection, $sql);
+    mysqli_stmt_execute($Statement);
+    $ReturnableResult = mysqli_stmt_get_result($Statement);
+    $record = mysqli_fetch_assoc($ReturnableResult);
+    $category = $record['StockGroupID'];
 
-$sql = "SELECT StockItemID from stockitemstockgroups WHERE StockGroupID = '$category'";
+    $sql = "SELECT StockItemID from stockitemstockgroups WHERE StockGroupID = '$category'";
 
-$result = mysqli_query($databaseConnection, $sql);
-$resultaat = array();
-$i = 0;
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-{
-    $StockItemID = $row["StockItemID"];
-    $resultaat[$i] = $StockItemID;
-    $i++;
-}
-echo "<table style='color: #EDF6FF; background-color: #6DAFFE; border-radius 10px; margin: auto'>";
-$aanbevelen = array_rand($resultaat, 5);
-    foreach($aanbevelen as $row){
+    $result = mysqli_query($databaseConnection, $sql);
+    $resultaat = array();
+    $i = 0;
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        $StockItemID = $row["StockItemID"];
+        $resultaat[$i] = $StockItemID;
+        $i++;
+    }
+    echo "<table style='color: #EDF6FF; background-color: #6DAFFE; border-radius 10px; margin: auto'>";
+    $aanbevelen = array_rand($resultaat, 5);
+    foreach ($aanbevelen as $row) {
         $sql = "SELECT S.StockItemName, S.RecommendedRetailPrice, I.ImagePath FROM stockitems S LEFT JOIN stockitemimages I on S.StockItemID = I.StockItemID WHERE S.StockItemID = '$row'";
         $result = mysqli_query($databaseConnection, $sql);
-            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                $StockItemName = $row['StockItemName'];
-                $RecommendedRetailPrice = $row['RecommendedRetailPrice'];
-                $ImagePath = $row['ImagePath'];
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $StockItemName = $row['StockItemName'];
+            $RecommendedRetailPrice = $row['RecommendedRetailPrice'];
+            $ImagePath = $row['ImagePath'];
 
-                $sql = "SELECT StockItemID FROM stockitems WHERE StockItemName = '$StockItemName'";
-                $Statement = mysqli_prepare($databaseConnection, $sql);
-                mysqli_stmt_execute($Statement);
-                $ReturnableResult = mysqli_stmt_get_result($Statement);
-                $record = mysqli_fetch_assoc($ReturnableResult);
-                $id2 = $record['StockItemID'];
+            $sql = "SELECT StockItemID FROM stockitems WHERE StockItemName = '$StockItemName'";
+            $Statement = mysqli_prepare($databaseConnection, $sql);
+            mysqli_stmt_execute($Statement);
+            $ReturnableResult = mysqli_stmt_get_result($Statement);
+            $record = mysqli_fetch_assoc($ReturnableResult);
+            $id2 = $record['StockItemID'];
 
-                echo "<tr>
+            echo "<tr>
                 <th><!--Name-->
                     <a style='color: #EDF6FF' href='http://localhost/nerdygadgets/view.php?id=$id2'>$StockItemName</a>
                 </th>
@@ -166,10 +164,11 @@ $aanbevelen = array_rand($resultaat, 5);
                 <form method='GET' action='Cart.php'>
                         <input type='hidden' name='target' value='$id2'>
                     </form>";
-            }
+        }
     }
-echo "</table>";
-
+    echo "</table>";
+}
+unset($_SESSION['id']);
 
 include __DIR__ . "/footer.php";
 ?>
